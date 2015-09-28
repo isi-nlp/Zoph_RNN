ff3b29a1b4a597d5fd11e1b922eafdb3d04e7c6b
Barret Zoph <barretzoph (at) gmail.com>
2015-09-28 16:01:16 -0700

    updates

--- src/compile_HPC.sh
+++ src/compile_HPC.sh
@@ -2,4 +2,4 @@ source /usr/usc/cuda/7.0/setup.sh
 source /usr/usc/boost/1.55.0/setup.sh
 source /usr/usc/gnu/gcc/4.8.1/setup.sh
 
-nvcc -O3 -Xcompiler -fopenmp -I /usr/usc/cuda/7.0/include/ -I /usr/usc/boost/1.55.0/include/ -I /home/nlg-05/zoph/eigen/ -std=c++11 -lcublas $1
+nvcc -O3 -Xcompiler -fopenmp -I /usr/usc/cuda/7.0/include/ -I /usr/usc/boost/1.55.0/include/     /usr/usc/boost/1.55.0/lib/libboost_system.a       /usr/usc/boost/1.55.0/lib/libboost_filesystem.a   /usr/usc/boost/1.55.0/lib/libboost_program_options.a -I /home/nlg-05/zoph/eigen/ -std=c++11 -lcublas main.cu
