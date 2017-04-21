51e3017399fe0d2f38616b656badc3264052c4e8
Shi Xing <shixing19910105@gmail.com>
2017-04-21 14:54:24 -0700

    support style configuration for fsaline

--- src/ensemble_factory.hpp
+++ src/ensemble_factory.hpp
@@ -82,8 +82,8 @@ void ensemble_factory<dType>::decode_file_interactive_line() {
     while (true) {
         // 1. source <source_file>  -> [END]
         // 2. words <words> -> [END]
-        // 3. fsa <fsa_file> enc1.txt,enc2.txt 1.0,-1.0 -> [END] : as normal
-        // 4. fsaline <fsa_file> enc1.txt,enc2.txt 1.0,-1.0 -> [END]: as noraml, but at the end, move corresponding ct and ht to all beams.
+        // 3. fsa <fsa_file> encourage_list_files:enc1.txt,enc2.txt encourage_weights:1.0,-1.0 repetition:0.0 alliteration:0.0 wordlen:0.0 -> [END] : as normal
+        // 4. fsaline <fsa_file> encourage_list_files:enc1.txt,enc2.txt encourage_weights:1.0,-1.0 repetition:0.0 alliteration:0.0 wordlen:0.0 -> [END]: as noraml, but at the end, move corresponding ct and ht to all beams.
         
         
         std::cout<<"Please input <source/words/fsa/fsaline> <source_file/words/fsa_file>\n";
@@ -201,18 +201,42 @@ void ensemble_factory<dType>::decode_file_interactive_line() {
             //process the encourage list file and encourage weight;
             std::vector<std::string> encourage_list_files;
             std::vector<float> encourage_weights;
+            float repetition_weight = 0.0;
+            float alliteration_weight = 0.0;
+            float wordlen_weight = 0.0;
 
-            if (ll.size() == 4) { // encourage
-                encourage_list_files = split(ll[2],',');
-                
-                std::vector<std::string> weight_strs = split(ll[3],',');
-                for (const std::string weight_str: weight_strs)
-                {
-                    encourage_weights.push_back(std::stof(weight_str));
+            for (int i = 2; i < ll.size(); i +=1){
+                std::vector<std::string> pair = split(ll[0],':');
+                if (pair[0] == "repetition"){
+                    repetition_weight = std::stof(pair[1]);
+                }
+                if (pair[0] == "alliteration"){
+                    alliteration_weight = std::stof(pair[1]);
+                }
+                if (pair[0] == "wordlen"){
+                    wordlen_weight = std::stof(pair[1]);
+                }
+                if (pair[0] == "encourage_list_files"){
+                    encourage_list_files = split(pair[1],',');
+                }
+                if (pair[0] == "encourage_weights"){
+                    std::vector<std::string> weight_strs = split(pair[1],',');
+                    for (const std::string weight_str: weight_strs)
+                    {
+                        encourage_weights.push_back(std::stof(weight_str));
+                    }
                 }
             }
-            model_decoder->init_encourage_lists(encourage_list_files,encourage_weights);
             
+            //process the encourage list file and encourage weight;
+            model_decoder->init_encourage_lists(encourage_list_files,encourage_weights);
+            //process repetition (>0 more repetition; <0 less repetition)
+            model_decoder->interactive_repeat_penalty = repetition_weight;
+            //process alliteration
+            model_decoder->alliteration_weight = alliteration_weight;
+            //process wordlen weight
+            model_decoder->wordlen_weight = wordlen_weight;
+
             
             
             decode_file_line(right_after_encoding,false);
@@ -251,18 +275,41 @@ void ensemble_factory<dType>::decode_file_interactive_line() {
             //process the encourage list file and encourage weight;
             std::vector<std::string> encourage_list_files;
             std::vector<float> encourage_weights;
+            float repetition_weight = 0.0;
+            float alliteration_weight = 0.0;
+            float wordlen_weight = 0.0;
             
-            if (ll.size() == 4) { // encourage
-                encourage_list_files = split(ll[2],',');
-                
-                std::vector<std::string> weight_strs = split(ll[3],',');
-                for (const std::string weight_str: weight_strs)
-                {
-                    encourage_weights.push_back(std::stof(weight_str));
+            for (int i = 2; i < ll.size(); i +=1){
+                std::vector<std::string> pair = split(ll[i],':');
+                if (pair[0] == "repetition"){
+                    repetition_weight = std::stof(pair[1]);
+                }
+                if (pair[0] == "alliteration"){
+                    alliteration_weight = std::stof(pair[1]);
+                }
+                if (pair[0] == "wordlen"){
+                    wordlen_weight = std::stof(pair[1]);
+                }
+                if (pair[0] == "encourage_list_files"){
+                    encourage_list_files = split(pair[1],',');
+                }
+                if (pair[0] == "encourage_weights"){
+                    std::vector<std::string> weight_strs = split(pair[1],',');
+                    for (const std::string weight_str: weight_strs)
+                    {
+                        encourage_weights.push_back(std::stof(weight_str));
+                    }
                 }
             }
+            
+            //process the encourage list file and encourage weight;
             model_decoder->init_encourage_lists(encourage_list_files,encourage_weights);
-
+            //process repetition (>0 more repetition; <0 less repetition)
+            model_decoder->interactive_repeat_penalty = repetition_weight;
+            //process alliteration
+            model_decoder->alliteration_weight = alliteration_weight;
+            //process wordlen weight
+            model_decoder->wordlen_weight = wordlen_weight;
             
             model_decoder->model = models[0].model;
             decode_file_line(right_after_encoding,true);
